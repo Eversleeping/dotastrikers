087822068d8ceb4ba790a4306fbad54a3efe3b34
Stephen <stephennf@gmail.com>
2015-07-05 00:13:59 -0400

    bugfix

--- game/dota_addons/dotastrikers/scripts/vscripts/myphysics.lua
+++ game/dota_addons/dotastrikers/scripts/vscripts/myphysics.lua
@@ -786,8 +786,8 @@ function DotaStrikers:SetupPersonalColliders(hero)
 				-- do negative stuff
 				if hero:GetTeam() ~= collided:GetTeam() then
 					Timers:CreateTimer(.03, function()
-						hero.tackleDummy:SetForwardVector((collided:GetAbsOrigin() - hero.tackleDummy:GetAbsOrigin()):Normalized())
-						--[[Timers:CreateTimer(.03, function()
+						--[[hero.tackleDummy:SetForwardVector((collided:GetAbsOrigin() - hero.tackleDummy:GetAbsOrigin()):Normalized())
+						Timers:CreateTimer(.03, function()
 							hero.tackleDummy:CastAbilityOnTarget(collided, hero.tackleDummyAbil, 0)
 							Timers:CreateTimer(.06, function()
 								--FireGameEvent("toggle_show_ability_silenced", {player_ID=collided:GetPlayerID(), ability_index=2})
@@ -796,7 +796,7 @@ function DotaStrikers:SetupPersonalColliders(hero)
 					end)
 
 					collided.last_time_tackled = GameRules:GetGameTime()
-					
+
 					AddMovementComponent(collided, "tackle", -9999)
 
 					-- give collided a small push
